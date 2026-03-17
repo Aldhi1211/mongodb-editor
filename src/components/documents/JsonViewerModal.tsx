56120260ad1fa7e23ff7f2d3fbe13ed4e1c46a28
Aldhi1211 <aldhitanca888@gmail.com>
2026-03-17 14:27:20 +0700

    Viewer modal ubah languagenya jadi json juga

--- src/components/documents/JsonViewerModal.tsx
+++ src/components/documents/JsonViewerModal.tsx
@@ -1,24 +1,36 @@
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
-import Editor from "@monaco-editor/react"
-import { toShellString } from "@/lib/ejsonShell"
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import Editor from "@monaco-editor/react";
+import { toShellString } from "@/lib/ejsonShell";
 
 export default function JsonViewerModal({ open, onClose, data }: any) {
-    return (
-        <Dialog open={open} onOpenChange={onClose}>
-            <DialogContent className="!max-w-none" style={{ width: "95vw", height: "90vh" }}>
-                <DialogHeader>
-                    <DialogTitle>Raw JSON Structure</DialogTitle>
-                </DialogHeader>
+  return (
+    <Dialog open={open} onOpenChange={onClose}>
+      <DialogContent
+        className="!max-w-none"
+        style={{ width: "95vw", height: "90vh" }}
+      >
+        <DialogHeader>
+          <DialogTitle>Raw JSON Structure</DialogTitle>
+        </DialogHeader>
 
-                <div className="flex-1 overflow-auto border rounded p-2 text-sm font-mono bg-muted">
-                    <Editor
-                        height="100%"
-                        defaultLanguage="javascript"
-                        value={toShellString(data)}
-                        options={{ readOnly: true, minimap: { enabled: false }, wordWrap: "on" }}
-                    />
-                </div>
-            </DialogContent>
-        </Dialog>
-    )
+        <div className="flex-1 overflow-auto border rounded p-2 text-sm font-mono bg-muted">
+          <Editor
+            height="100%"
+            defaultLanguage="json"
+            value={toShellString(data)}
+            options={{
+              readOnly: true,
+              minimap: { enabled: false },
+              wordWrap: "on",
+            }}
+          />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
 }
